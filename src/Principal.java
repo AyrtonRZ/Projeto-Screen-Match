6eefa905937a10a5f0f075549bdc4a8538d7d18e
AyrtonRZ <ayrtonrodrigues45@gmail.com>
2023-08-29 11:09:26 -0300

    feat: adiciona chamada dos metodos na classe principal

--- src/Principal.java
+++ src/Principal.java
@@ -4,9 +4,14 @@ public class Principal {
         filme01.nomeFilme = "O poderoso Chefao";
         filme01.anoLancamento = 1987;
         filme01.duracaoMinutos = 180;
+        filme01.incluidoPlano = true;
 
-        System.out.println(filme01.nomeFilme);
-        System.out.println(filme01.anoLancamento);
-        System.out.println(filme01.duracaoMinutos);
+        filme01.exibeInfomacaoFilme();
+
+        filme01.avaliacao(9.9);
+        filme01.avaliacao(8.5);
+        filme01.avaliacao(7.9);
+
+        System.out.println("Media de avaliacao do filme: "+filme01.getSomaAvaliacao());
     }
 }
