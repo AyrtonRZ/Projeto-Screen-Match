fd14d2ec66b4d0d386d2fbefdcbc9899e28160ab
AyrtonRZ <ayrtonrodrigues45@gmail.com>
2023-08-29 20:57:13 -0300

    feat: adiciona classes Titulos e Series aplicando herancas

--- src/Principal.java
+++ src/Principal.java
@@ -1,20 +1,34 @@
 import br.com.alura.screenmatch.modelo.Filme;
+import br.com.alura.screenmatch.modelo.Serie;
 
 public class Principal {
     public static void main(String[] args) {
         Filme filme01 = new Filme();
-        filme01.setNomeFilme("O poderoso Chefao");
+
+        Serie serie01 = new Serie();
+
+        //filmes
+        filme01.setNome("O poderoso Chefao");
         filme01.setAnoLancamento(1987);
         filme01.setDuracaoMinutos(160);
         filme01.setIncluidoPlano(true);
-
-        filme01.exibeInfomacaoFilme();
 
         filme01.avaliacao(9.9);
         filme01.avaliacao(8.5);
         filme01.avaliacao(7.9);
 
+        filme01.exibeInfomacao();
         System.out.println("Total de Avaliacoes: "+filme01.getTotalAvaliacao());
         System.out.println("Media de avaliacao do filme: "+filme01.getSomaAvaliacao());
+
+        //series
+        serie01.setNome("Lost");
+        serie01.setAnoLancamento(2002);
+        serie01.setTemporadas(10);
+        serie01.setEpisodioPorTemporada(24);
+        serie01.setAtiva(false);
+
+        serie01.exibeInfomacao();
+
     }
 }
---
+++ src/br/com/alura/screenmatch/modelo/Titulo.java
@@ -0,0 +1,60 @@
+package br.com.alura.screenmatch.modelo;
+
+public class Titulo {
+    private String nome;
+    private int anoLancamento;
+    private boolean incluidoPlano;
+    private double somaAvaliacao;
+    private int totalAvaliacao;
+    private int duracaoMinutos;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public boolean isIncluidoPlano() {
+        return incluidoPlano;
+    }
+
+    public int getDuracaoMinutos() {
+        return duracaoMinutos;
+    }
+
+    public int getTotalAvaliacao(){
+        return totalAvaliacao;
+    }
+
+    public double getSomaAvaliacao(){
+        return somaAvaliacao / totalAvaliacao;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public void setIncluidoPlano(boolean incluidoPlano) {
+        this.incluidoPlano = incluidoPlano;
+    }
+
+    public void setDuracaoMinutos(int duracaoMinutos) {
+        this.duracaoMinutos = duracaoMinutos;
+    }
+
+    public void exibeInfomacao(){
+        System.out.println("Nome do Filme: "+getNome());
+        System.out.println("Ano de Lancamento: "+getAnoLancamento());
+    }
+
+    public void avaliacao(double nota){
+        somaAvaliacao += nota;
+        totalAvaliacao ++;
+    }
+}
